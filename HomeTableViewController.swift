c2bd6c574a9927b4d865b2f75fdeb45ad70a70e3
alanf01 <91225704+alanf01@users.noreply.github.com>
2022-10-04 20:13:27 -0400

    Update HomeTableViewController.swift

--- HomeTableViewController.swift
+++ HomeTableViewController.swift
@@ -68,6 +68,12 @@ class HomeTableViewController: UITableViewController {
         
         cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
         
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        
         
         
         
